84a32741eaff9c97975326f36485a38ac46d3b69
AlperenErdogan <alperenerdogan0505@gmail.com>
2021-04-01 16:05:05 +0300

    Managers updated

---
+++ Assets/Scripts/Managers/Scriptables/ScriptableGameSettings.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Manager/GameSettings")]
+public class ScriptableGameSettings : AbstractScriptableObject
+{
+    [ReadOnlyInInspector] [SerializeField] private int screenWidth;
+    public int ScreenWidth { get { return screenWidth; } }
+    [ReadOnlyInInspector] [SerializeField] private int screenHeight;
+    public int ScreenHeight { get { return screenHeight; } }
+    [SerializeField] private int gridSize;
+    public int GridSize { get { return gridSize; } }
+    [Header("Layers")]
+    [SerializeField] private int groundLayer;
+    public int GroundLayer { get { return groundLayer; } }
+
+    public override void Initialize()
+    {
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+    }
+}
