966c6d9f9fcb10a4e22e17dd528eae9067862aa2
alperen <alperenerdogan0505@gmail.com>
2021-05-02 06:25:24 +0300

    UI Manager added

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text selectedObjectNameText;
+
+    void Start()
+    {
+        selectedObjectNameText.text = "Selected: Nothing";
+    }
+    public void ChangeSelectedText(string selectedObjectName)
+    {
+        selectedObjectNameText.text = "Selected: " + selectedObjectName;
+
+    }
+}
